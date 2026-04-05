cfad74200d3c4caf10f3b7da78e753fbfad1711a
Ankit Karn <96945118+ankit3954@users.noreply.github.com>
2026-04-05 11:11:09 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        if(s.length() % 2 != 0){
+            return false;
+        }
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+           if (c == '(') {
+                st.push(')');
+            } else if (c == '{') {
+                st.push('}');
+            } else if (c == '[') {
+                st.push(']');
+            }else{
+                 if (st.isEmpty() || st.pop() != c) {
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
