a23d6ab962ec36bd37848d79b43d389bc8e6f7af
Ankit Karn <96945118+ankit3954@users.noreply.github.com>
2026-04-27 21:52:05 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        // int maxCount = Integer.MIN_VALUE, res = -1, count = 0;
+        int res = -1;
+        for(int i = 0, j = 1; j < nums.length; i++, j++){
+            if(nums[i] == nums[j]){
+                // count++;
+                // maxCount = Math.max(maxCount, count);
+                res = nums[i];
+            }
+            // else{
+            //     // count = 1;
+            //     maxCount =  Math.max(maxCount, count);
+            // }
+        }
+
+        return res;
+    }
+}
