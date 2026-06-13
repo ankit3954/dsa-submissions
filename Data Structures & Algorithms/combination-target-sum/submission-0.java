ee344114086d1bdd01d45e680fce37394fc6863f
Ankit Karn <96945118+ankit3954@users.noreply.github.com>
2026-06-13 11:08:00 +0530

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.java
@@ -0,0 +1,42 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> combination = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        int sum = 0;
+        combinationSumHelper(nums, target, sum);
+
+        return ans;
+    }
+
+    public int findSum(List<Integer> comb){
+        int sum = 0;
+        for(int i = 0; i < comb.size(); i++){
+            sum += comb.get(i);
+        }
+
+        return sum;
+    }
+
+    public void combinationSumHelper(int[] nums, int target, int sum) {
+        if(sum > target) return;
+
+        if(sum == target){
+            ans.add(new ArrayList<>(combination));
+            return;
+        }
+
+
+        for(int i = 0; i < nums.length; i++){
+            if(combination.size() > 0 && combination.getLast() > nums[i]){
+                continue;
+            }
+            combination.add(nums[i]);
+            sum = findSum(combination);
+            combinationSumHelper(nums, target, sum);
+            combination.removeLast();
+        }
+
+    }
+
+}
