1649389a6b94debde73eddc3ab9c1f7ba468028b
Ankit Karn <96945118+ankit3954@users.noreply.github.com>
2026-06-15 11:08:27 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        HashMap<Character, String> map = new HashMap<>();
+        String arr[] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        for(int i = 2; i < 10; i++){
+            char key = (char) (i + '0');
+            map.putIfAbsent(key, arr[i-2]);
+        }
+
+        return letterCombinationsHelper(digits, map);
+    }
+
+    public List<String> letterCombinationsHelper(String digits, HashMap<Character, String> map) {
+        if(digits.length() == 0){
+            List<String> ans = new ArrayList<>();
+            return ans;
+        }
+
+        List<String> res = letterCombinationsHelper(digits.substring(1), map);
+        if(res.size() == 0){
+            System.out.print("hello");
+            res.add("");
+        }
+        String s = map.get(digits.charAt(0));
+        List<String> ans = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++){
+            for(int j = 0; j < res.size(); j++){
+                String finalString = s.charAt(i) + res.get(j);
+                System.out.print(finalString + " ");
+                ans.add(finalString);
+            }
+
+        }
+        System.out.println();
+        return ans;
+    
+    }
+
+}
