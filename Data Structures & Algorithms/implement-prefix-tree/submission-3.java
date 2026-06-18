5a648c85083970708c09696554e348ad8bc32070
Ankit Karn <96945118+ankit3954@users.noreply.github.com>
2026-06-18 11:18:52 +0530

    Add: implement-prefix-tree - submission-3

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-3.java
@@ -0,0 +1,81 @@
+ class TrieNode{
+        char data;
+        TrieNode[] children;
+        boolean isTerminal;
+
+        public TrieNode(char data){
+            this.data = data;
+            children = new TrieNode[26];
+            isTerminal = false;
+        }
+    }
+
+class PrefixTree {
+    TrieNode root;
+    public PrefixTree() {
+         root = new TrieNode('/');
+    }
+
+    public void insert(String word) {
+        insertHelper(root, word);
+    }
+
+    public void insertHelper(TrieNode root, String word) {
+        if(word.length() == 0){
+            root.isTerminal = true;
+            return;
+        }
+
+        int index = word.charAt(0) - 'a';
+        TrieNode child;
+        if(root.children[index] != null){
+            child = root.children[index];
+        }else{
+            child = new TrieNode(word.charAt(0));
+            root.children[index] = child;
+        }
+
+        insertHelper(child, word.substring(1));
+    }
+
+    public boolean search(String word) {
+        return searchHelper(root, word);
+    }
+
+    public boolean searchHelper(TrieNode root, String word){
+        if(word.length() == 0){
+            return root.isTerminal;
+        }
+
+        int index = word.charAt(0) - 'a';
+        TrieNode child;
+
+        if(root.children[index] != null && root.children[index].data == word.charAt(0)){
+            child = root.children[index];
+            return searchHelper(child, word.substring(1));
+        }else{
+            return false;
+        }
+    }
+
+    public boolean startsWith(String prefix) {
+        return startsWithHelper(root, prefix);
+    }
+    public boolean startsWithHelper(TrieNode root, String prefix) {
+        if(prefix.length() == 0){
+            return true;
+        }
+
+        int index = prefix.charAt(0) - 'a';
+        TrieNode child;
+
+        if(root.children[index] != null && root.children[index].data == prefix.charAt(0)){
+            child = root.children[index];
+            return startsWithHelper(child, prefix.substring(1));
+        }else{
+            return false;
+        }
+    }
+
+   
+}
