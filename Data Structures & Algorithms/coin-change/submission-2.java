3f30360d61e9a44512029a2e804c29206ef331ed
Ankit Karn <96945118+ankit3954@users.noreply.github.com>
2026-09-01 11:46:35 +0545

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+
+        for(int i = 0; i < amount + 1; i++){
+            dp[i] = amount+1;
+        }
+        dp[0] = 0;
+        for(int i = 1; i < amount + 1; i++){
+            for(int j = 0; j < coins.length; j++){
+                if(coins[j] <= i){
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                }
+            }
+        }
+        for(int i = 0; i < dp.length; i++){
+            System.out.print(dp[i] + " ");
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
